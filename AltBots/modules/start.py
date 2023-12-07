24dda3d2ae5420cac38f75496c400fe606e8d8c4
CUTIEEBOY9 <152642869+CUTIEEBOY9@users.noreply.github.com>
2023-12-07 07:33:44 -0800

    Update start.py

--- AltBots/modules/start.py
+++ AltBots/modules/start.py
@@ -8,11 +8,11 @@ START_BUTTON = [
         Button.inline("💘 𝙲𝚘𝚖𝚖𝚊𝚗𝚍𝚂 💘", data="help_back")
     ],
     [
-        Button.url("🌺 𝙳𝚎𝚟𝚎𝚕𝚘𝚙𝚎𝚁 🌺", "https://t.me/moon_m_6"),
-        Button.url("🌸 𝚂𝚞𝚙𝚙𝚘𝚛𝚃 🌸", "https://t.me/+PfRardqZ41U1MTg1")
+        Button.url("🌺 𝙳𝚎𝚟𝚎𝚕𝚘𝚙𝚎𝚁 🌺", "https://t.me/CUTIEEBOY9"),
+        Button.url("🌸 𝚂𝚞𝚙𝚙𝚘𝚛𝚃 🌸", "https://t.me/CRAZY_FRIENDSS_CHATTING")
     ],
     [
-        Button.url("💖𓄂➻❰ 𝐒𝐍𝐗™❱┼ꭙ‌⚡️「𝐌𝓞𝓞𝐍」⚡️💖", "https://t.me/moon_m_6")
+        Button.url("༄𝗖𝗨𝗧𝗘 𝗕𝗢𝗬", "https://t.me/CUTIEEBOY9")
 
         
     ]
@@ -36,13 +36,13 @@ async def start(event):
         bot_name = AltBot.first_name
         bot_id = AltBot.id
         TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})​**\n━━━━━━━━━━━━━━━━━━━\n\n"
-        TEXT += f"» **𝙼𝚢 𝙳𝙴𝚅𝙴𝙻𝙾𝙿𝙴𝚁 : 𓄂➻❰ 𝐒𝐍𝐗™❱┼ꭙ‌⚡️「𝐌𝓞𝓞𝐍」⚡️**\n\n"
+        TEXT += f"» **𝙼𝚢 𝙳𝙴𝚅𝙴𝙻𝙾𝙿𝙴𝚁 : ༄𝗖𝗨𝗧𝗘 𝗕𝗢𝗬**\n\n"
         TEXT += f"» **𝙱𝙾𝚃𝚂 𝚅𝙴𝚁𝚂𝙸𝙾𝙽 :** `M3.3`\n"
         TEXT += f"» **𝙿𝚈𝚃𝙷𝙸𝙽 𝚅𝙴𝚁𝚂𝙸𝙾𝙽 :** `3.11.3`\n"
         TEXT += f"» **MOON ᴠᴇʀsɪᴏɴ :** `{__version__}`\n━━━━━━━━━━━━━━━━━"
         await event.client.send_file(
                     event.chat_id,
-                    "https://telegra.ph/file/d557c20c6054491d2c20a.jpg",
+                    "https://telegra.ph/file/c4b8f21ac95647ecd37ed.jpg",
                     caption=TEXT, 
                     buttons=START_BUTTON
                 )
